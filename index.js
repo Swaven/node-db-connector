e9e3d1e47393c05b1bbf99bd88be5d580e90b98f
Antoine Goutagny <antoine@swaven.com>
2021-11-25 17:40:04 +0100

    fix require, bump version

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ class DbConnector {
     // find redis configs
     var redisConfigs = configs.filter((x) => {return x.connectionString.startsWith('redis://')})
     if (redisConfigs.length > 0){
-      let redis = require('promise-redis')()
+      let redis = require('promise-redis-legacy')()
       for (let cfg of redisConfigs){
         this._connectRedis(cfg, redis)
       }

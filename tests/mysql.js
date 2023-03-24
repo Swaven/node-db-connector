d6c37dfe328618666b8993daad837d9a38b05cfd
Antoine Goutagny <antoine@swaven.com>
2023-03-24 09:30:57 +0100

    Add mysql & redis tests
    + rename existing test file

---
+++ tests/mysql.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const chai = require('chai'),
+      assert = chai.assert,
+      { readFileSync } = require('fs')
+
+
+const connStrings = JSON.parse(readFileSync('tests/connectionStrings.json'))
+
+// Fail if connection strings contain placeholder values instead of actual credentials
+const invalid = Object.values(connStrings).some(x => x.includes('PWD@'))
+if (invalid){
+  console.error('Invalid connection strings')
+  return
+}
+
+describe("MySQL", () => {
+  const sut = require('../index.js')
+  it('connect', async () => {
+    await sut.init([{name: 'stg_cms', connectionString: connStrings.mysql_staging}])
+    assert.isNotNull(sut.stg_cms)
+  })
+  it('close', async () => {
+    await sut.close()
+  })
+})
+
